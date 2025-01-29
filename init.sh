ae981e047e2d4b0c10e26b9ba4fc4d61ddbb2cac
Maxwell Machado <34950484+maxwellwolf@users.noreply.github.com>
2025-01-29 18:01:36 -0300

    Update init.sh
    Ajuste no src da criação do container www

--- init.sh
+++ init.sh
@@ -13,4 +13,4 @@ docker rm db --force
 docker run --name db --network=sre_default -e MYSQL_ROOT_PASSWORD=987654321 -e MYSQL_USER=admin -e MYSQL_PASSWORD=admin -e MYSQL_DATABASE=SRE -p 3306:3306 -d db_mysql --default-authentication-plugin=mysql_native_password
 
 docker rm www --force
-docker run --name www --mount type=bind,src=/home/ec2-user/SRE,dst=/var/www/html,ro --network=sre_default --link db:db -p 80:80 -d php_apache
+docker run --name www --mount type=bind,src=.,dst=/var/www/html,ro --network=sre_default --link db:db -p 80:80 -d php_apache
